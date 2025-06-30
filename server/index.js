ddd253ba6aecb5fd22e7d2382dff95a13bf03637
duaa-mustafa <aduaa360@gmail.com>
2025-06-30 12:24:50 +0300

    Fix backend: replace React frontend code with Express server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { AssignmentsProvider } from './AssignmentsContext';
+// server/index.js
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AssignmentsProvider>
-    <App />
-    </AssignmentsProvider>
-  </React.StrictMode>
-);
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+// You can add more routes here, e.g., API endpoints
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
